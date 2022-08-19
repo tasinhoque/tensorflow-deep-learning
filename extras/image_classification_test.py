dcfd740da09484898e5b5face49cf4e94dd1c83a
Tasin Hoque <tasinhoque@gmail.com>
2022-08-19 12:32:51 +0600

    Format

--- extras/image_classification_test.py
+++ extras/image_classification_test.py
@@ -15,45 +15,54 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers
 
 # Check version of TensorFlow (exam requires a certain version)
-# See for version: https://www.tensorflow.org/extras/cert/Setting_Up_TF_Developer_Certificate_Exam.pdf 
+# See for version: https://www.tensorflow.org/extras/cert/Setting_Up_TF_Developer_Certificate_Exam.pdf
 print(tf.__version__)
 
 # Get data
-(train_images, train_labels), (test_images, test_labels) = datasets.fashion_mnist.load_data()
+(train_images, train_labels), (
+    test_images,
+    test_labels,
+) = datasets.fashion_mnist.load_data()
 
 # Normalize images (get values between 0 & 1)
-train_images, test_images = train_images / 255.0, test_images / 255.0 
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
 # Check shape of input data
 # print(train_images.shape)
 # print(train_labels.shape)
 
 # Build model
-model = tf.keras.Sequential([
-    # Reshape inputs to be compatible with Conv2D layer
-    layers.Lambda(lambda x: tf.expand_dims(x, axis=-1)),
-    layers.Conv2D(32, 3, activation="relu"),
-    layers.MaxPool2D(),
-    layers.Conv2D(32, 3, activation="relu"),
-    layers.MaxPool2D(),
-    layers.Conv2D(32, 3, activation="relu"),
-    layers.Flatten(), # flatten outputs of final Conv layer to be suited for final Dense layer
-    layers.Dense(10, activation="softmax")
-])
+model = tf.keras.Sequential(
+    [
+        # Reshape inputs to be compatible with Conv2D layer
+        layers.Lambda(lambda x: tf.expand_dims(x, axis=-1)),
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.MaxPool2D(),
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.MaxPool2D(),
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.Flatten(),  # flatten outputs of final Conv layer to be suited for final Dense layer
+        layers.Dense(10, activation="softmax"),
+    ]
+)
 
-# Compile model 
-model.compile(loss="sparse_categorical_crossentropy", # if labels aren't one-hot use sparse (if labels are one-hot, drop sparse)
-              optimizer=tf.keras.optimizers.Adam(),
-              metrics=["accuracy"])
+# Compile model
+model.compile(
+    loss="sparse_categorical_crossentropy",  # if labels aren't one-hot use sparse (if labels are one-hot, drop sparse)
+    optimizer=tf.keras.optimizers.Adam(),
+    metrics=["accuracy"],
+)
 
 # Fit model
 print("Training model...")
-model.fit(x=train_images,
-          y=train_labels,
-          epochs=10,
-          validation_data=(test_images, test_labels))
+model.fit(
+    x=train_images,
+    y=train_labels,
+    epochs=10,
+    validation_data=(test_images, test_labels),
+)
 
-# Evaluate model 
+# Evaluate model
 print("Evaluating model...")
 model.evaluate(test_images, test_labels)
 
